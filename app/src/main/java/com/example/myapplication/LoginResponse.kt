d61e7dd293d08959ab32b2ee12c11e1faf41df55
HaeRyeong Jin <jhr5559@gmail.com>
2025-10-31 11:30:07 +0900

    Feat: 생년월일 입력 추가

--- app/src/main/java/com/example/myapplication/LoginResponse.kt
+++ app/src/main/java/com/example/myapplication/LoginResponse.kt
@@ -3,5 +3,6 @@ package com.example.myapplication
 data class LoginResponse(
     val accessToken: String,
     val userId: Long,
-    val username: String
+    val username: String,
+    val gender: String
 )
--- app/src/main/java/com/example/myapplication/UserRegistrationRequest.kt
+++ app/src/main/java/com/example/myapplication/UserRegistrationRequest.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication.data.request
 
+import java.util.Date
+
 data class UserRegistrationRequest(
     val loginId: String,
     val password: String,
     val username: String,
     val gender: String,
+    val birthday: String,
     val caregiverId: Long?
 )
